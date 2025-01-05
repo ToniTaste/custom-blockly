29c0bce033a3f113d9ec8cf780f1ae9aab2e9725
ToniTaste <feedback@tinohempel.de>
2025-01-05 10:48:46 +0100

    Update

--- app.js
+++ app.js
@@ -6,5 +6,41 @@ const workspace = Blockly.inject('blocklyDiv', {
             <block type="math_number"></block>
             <block type="text"></block>
         </xml>
+		
+		<xml>
+    <category name="Basics">
+        <block type="math_number"></block>
+        <block type="text"></block>
+    </category>
+    <category name="Custom">
+        <block type="print_message"></block>
+    </category>
+</xml>
+
     `,
 });
+
+// Blockdefinition
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "print_message",
+        "message0": "Print %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "TEXT"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 160,
+        "tooltip": "Print a message",
+        "helpUrl": ""
+    }
+]);
+
+// Generator
+Blockly.JavaScript['print_message'] = function(block) {
+    const text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_ATOMIC);
+    return `console.log(${text});\n`;
+};
